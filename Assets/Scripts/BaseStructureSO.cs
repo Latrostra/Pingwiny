345ae4069603ef1fb35ecc7ee464e5bf11ab69fe
Latrostra <43313391+Latrostra@users.noreply.github.com>
2020-10-12 18:41:29 +0200

    DataLayer

---
+++ Assets/Scripts/BaseStructureSO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseStructureSO : ScriptableObject {
+    public GameObject prefab;
+    public string buildName;
+    public int cost;
+    public int upkeep;
+    public int income;
+}
---
+++ Assets/Scripts/SingleStructureSO.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName="New Single Structure", menuName="Structure/StructureData/SingleStructure")]
+public class SingleStructureSO : BaseStructureSO
+{
+    public GameObject[] gameObjectVariant;
+    public bool upgradable;
+    public UpgradeType[] availableUpgrades;
+}
+
+[System.Serializable]
+public struct UpgradeType {
+    public GameObject[] upgradeVarians;
+    public int newIncome;
+    public int newUpkeep;
+}
